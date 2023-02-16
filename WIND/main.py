5d354fbafa622d431b988434efedd59fdfafa027
nicolaf03 <nicola.fraccarolo@gmail.com>
2023-02-16 18:43:08 +0100

    aggiunto train e test al main, todo: modificare loss

--- WIND/main.py
+++ WIND/main.py
@@ -1,9 +1,14 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import torch.backends.cudnn as cudnn
 
 import argparse
 import random
+import os
+
 import data_loader
+import models
 
 parser = argparse.ArgumentParser(description='SDE TCNN Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate of drift net')
@@ -42,3 +47,101 @@ train_loader, test_loader = data_loader.getDataSet(args.zone, args.H, args.h, ar
 print('==> Building model..')
 net = models.SDENet_wind()
 net = net.to(device)
+
+# criterion = nn.CrossEntropyLoss()
+# criterion2 = nn.BCELoss()
+
+optimizer_F = optim.SGD(
+    params=[{'params': net.downsampling_layers.parameters()}, {'params': net.drift.parameters()}, {'params': net.fc_layers.parameters()}],
+    lr=args.lr,
+    momentum=0.9,
+    weight_decay=5e-4
+)
+optimizer_G = optim.SGD(
+    params=[{'params': net.diffusion.parameters()}],
+    lr=args.lr2,
+    momentum=0.9,
+    weight_decay=5e-4
+)
+
+# use a smaller sigma during training for training stability
+net.sigma = 20
+
+
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+
+    train_loss = 0
+    correct = 0
+    total = 0
+    train_loss_out = 0
+    train_loss_in = 0
+
+    ##training with in-domain data
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer_F.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer_F.step()
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+    #training with out-of-domain data
+        label = torch.full((args.batch_size,1), float(real_label), device=device)
+        optimizer_G.zero_grad()
+        predict_in = net(inputs, training_diffusion=True)
+        loss_in = criterion2(predict_in, label)
+        loss_in.backward()
+        label.fill_(fake_label)
+        inputs_out = 2*torch.randn(args.batch_size,1, args.imageSize, args.imageSize, device = device)+inputs
+        predict_out = net(inputs_out, training_diffusion=True)
+        loss_out = criterion2(predict_out, label)
+        loss_out.backward()
+        train_loss_out += loss_out.item()
+        train_loss_in += loss_in.item()
+        optimizer_G.step()
+
+    print('Train epoch:{} \tLoss: {:.6f} | Loss_in: {:.6f}, Loss_out: {:.6f} | Acc: {:.6f} ({}/{})'
+        .format(epoch, train_loss/(len(train_loader)), train_loss_in/len(train_loader), train_loss_out/len(train_loader), 100.*correct/total, correct, total))
+
+
+def test(epoch):
+    net.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(test_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = 0
+            for j in range(args.eva_iter):
+                current_batch = net(inputs)
+                outputs = outputs + F.softmax(current_batch, dim = 1)
+
+            outputs = outputs/args.eva_iter
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+        print('Test epoch: {} | Acc: {:.6f} ({}/{})'
+        .format(epoch, 100.*correct/total, correct, total))
+
+
+for epoch in range(0, args.epochs):
+    train(epoch)
+    test(epoch)
+    if epoch in args.decreasing_lr:
+        for param_group in optimizer_F.param_groups:
+            param_group['lr'] *= args.droprate
+    if epoch in args.decreasing_lr2:
+        for param_group in optimizer_G.param_groups:
+            param_group['lr'] *= args.droprate
+
+if not os.path.isdir('./save_sdenet_wind'):
+    os.makedirs('./save_sdenet_wind')
+torch.save(net.state_dict(),'./save_sdenet_wind/final_model')
+
