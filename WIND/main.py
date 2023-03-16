03e4b6def9ddecc40cbb69327ebfe32a76bafbc0
nicolaf03 <nicola.fraccarolo@gmail.com>
2023-03-16 14:03:21 +0100

    allenato modello con feed forward

--- WIND/main.py
+++ WIND/main.py
@@ -20,22 +20,24 @@ def main():
     parser = argparse.ArgumentParser(description='SDE CNN Training')
     parser.add_argument('--lr', default=0.01, type=float, help='learning rate of drift net')
     parser.add_argument('--lr2', default=0.01, type=float, help='learning rate of diffusion net')
-    parser.add_argument('--training_out', action='store_false', default=True, help='training_with_out')
+    # parser.add_argument('--training_out', action='store_false', default=True, help='training_with_out')
     parser.add_argument('--epochs', type=int, default=40, help='number of epochs to train')
-    parser.add_argument('--eva_iter', default=1, type=int, help='number of passes when evaluation')
+    # parser.add_argument('--eva_iter', default=1, type=int, help='number of passes when evaluation')
     #
     parser.add_argument('--zone', default='mock', help='zone')
     parser.add_argument('--h', default=1, help='time horizon forecasting')
-    parser.add_argument('--H', default=28, help='length of history')
+    parser.add_argument('--H', default=100, help='length of history')
     #
     parser.add_argument('--batch_size', type=int, default=1, help='input batch size for training')
     parser.add_argument('--test_batch_size', type=int, default=1)
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--seed', type=float, default=0)
     parser.add_argument('--droprate', type=float, default=0.1, help='learning rate decay')
-    parser.add_argument('--decreasing_lr', default=[10, 20, 30], nargs='+', help='decreasing strategy')
-    parser.add_argument('--decreasing_lr2', default=[15, 30], nargs='+', help='decreasing strategy')
+    parser.add_argument('--decreasing_lr', default=[20], nargs='+', help='decreasing strategy')
+    parser.add_argument('--decreasing_lr2', default=[], nargs='+', help='decreasing strategy')
+    
     args = parser.parse_args()
+    print(args)
 
     # check if gpu is available
     device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
@@ -52,31 +54,27 @@ def main():
 
     # Model
     print('==> Building model..')
-    net = models.SDENet_wind(layer_depth=6, dim=64)
+    net = models.SDENet_wind(layer_depth=4, H=args.H)
     net = net.to(device)
 
 
     real_label = 0
     fake_label = 1
 
-    # todo: cambiare loss
-    criterion = nn.L1Loss()     
+    # criterion = nn.L1Loss()     
     # criterion = nn.GaussianNLLLoss()
-    criterion2 = nn.BCELoss()
+    criterion = nn.BCELoss()
     
     def nll_loss(y, mean, sigma):
         loss = torch.mean(torch.log(sigma) + (y - mean)**2 / (sigma**2))
         return loss
-    
-    # def mse(y_pred, y_true):
-    #     loss = torch.mean((y_pred - y_true)**2)
-    #     return loss
-    
-    def mape(y_pred, y_true):
-        loss = torch.mean(torch.abs(y_pred - y_true) / y_true)
+    def mse(y_pred, y_true):
+        loss = torch.mean((y_pred - y_true)**2)
         return loss
-    
-    mse = nn.MSELoss()
+    # def mape(y_pred, y_true):
+    #     loss = torch.mean(torch.abs(y_pred - y_true) / y_true)
+    #     return loss
+    # mse = nn.MSELoss()
 
     optimizer_F = optim.SGD(
         params=[
@@ -86,8 +84,8 @@ def main():
             {'params': net.diffusion.parameters()}
         ],
         lr=args.lr,
-        momentum=0.9,
-        # weight_decay=5e-4
+        momentum=0.5,
+        weight_decay=5e-4
     )
     optimizer_G = optim.SGD(
         params=[{'params': net.diffusion.parameters()}],
@@ -105,70 +103,72 @@ def main():
     #     lr=0.001
     # )
     # use a smaller sigma during training for training stability
-    net.sigma = 20
+    # net.sigma = 20
 
     # training
     def train(epoch):
         print('\nEpoch: %d' % epoch)
         net.train()
+        
+        if epoch == 0:
+            net.sigma = 0.1
+        if epoch == 30:
+            net.sigma = 0.5
 
         train_loss = 0
-        correct = 0
-        total = 0
-        train_loss_out = 0
         train_loss_in = 0
-
+        train_loss_out = 0
         for batch_idx, (inputs, targets) in enumerate(train_loader):
             #
             # training with in-domain data
             optimizer_F.zero_grad()
             
-            inputs = inputs.to(device)                          #[128, 1, 28]
-            targets = targets.to(device)                        #[128, 1]
-            # mu, sigma = net(inputs, training_diffusion=False)   #[128, 1]
-            predict = net(inputs, training_diffusion=False)     #[128, 1]
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            mu, sigma = net(inputs, training_diffusion=False)
+            # predict = net(inputs, training_diffusion=False)
             
-            # loss = nll_loss(targets, mu, sigma)                 # risolto
-            loss = mse(predict, targets)
+            loss = nll_loss(targets, mu, sigma)
+            # loss = mse(predict, targets)
             loss.backward()
             #pdb.set_trace()
+            nn.utils.clip_grad_norm_(net.parameters(), 100.)
             train_loss += loss.item()
             
             optimizer_F.step()
-            '''
-            #
-            # training with out-of-domain data
-            optimizer_G.zero_grad()
+            
+            # #
+            # # training with out-of-domain data
+            # optimizer_G.zero_grad()
         
-            # tensor full of zeros
-            label = torch.full((args.batch_size,1), float(real_label), device=device)
-            predict_in = net(inputs, training_diffusion=True)
-            #
-            # distance between non-noisy data and 0
-            # binary cross entropy because there are only 2 classes: 0 (no noise) and 1 (maximum noise)
-            loss_in = criterion2(predict_in, label)
-            loss_in.backward()
-            train_loss_in += loss_in.item()
+            # # tensor full of zeros
+            # label = torch.full((args.batch_size,1), float(real_label), device=device)
+            # predict_in = net(inputs, training_diffusion=True)
+            # #
+            # # distance between non-noisy data and 0
+            # # binary cross entropy because there are only 2 classes: 0 (no noise) and 1 (maximum noise)
+            # loss_in = criterion(predict_in, label)
+            # loss_in.backward()
+            # train_loss_in += loss_in.item()
             
-            # tensor full of ones
-            label.fill_(fake_label)
-            inputs_out = inputs + 2 * torch.randn(args.batch_size, 1, args.H, device=device)
-            predict_out = net(inputs_out, training_diffusion=True)
-            #
-            # distance between noisy data and 1
-            # binary cross entropy because there are only 2 classes: 0 (no noise) and 1 (maximum noise)
-            loss_out = criterion2(predict_out, label)
-            loss_out.backward()
-            train_loss_out += loss_out.item()
+            # # tensor full of ones
+            # label.fill_(fake_label)
+            # inputs_out = inputs + 2 * torch.randn(args.batch_size, 1, args.H, device=device)
+            # predict_out = net(inputs_out, training_diffusion=True)
+            # #
+            # # distance between noisy data and 1
+            # # binary cross entropy because there are only 2 classes: 0 (no noise) and 1 (maximum noise)
+            # loss_out = criterion(predict_out, label)
+            # loss_out.backward()
+            # train_loss_out += loss_out.item()
             
-            optimizer_G.step()
-            '''
+            # optimizer_G.step()
+            
 
-        # print('Train epoch: {} \tLoss: {:.6f} | Loss_in: {:.6f}, Loss_out: {:.6f}'
-        #     .format(epoch, train_loss/(len(train_loader)), train_loss_in/len(train_loader), train_loss_out/len(train_loader)))
-        
         print('Train epoch: {} \tLoss: {:.6f}'
         .format(epoch, train_loss/(len(train_loader))))
+        # print('Train epoch: {} \tLoss: {:.6f} | Loss_in: {:.6f} | Loss_out: {:.6f}'
+        # .format(epoch, train_loss/(len(train_loader)), train_loss_in/(len(train_loader)), train_loss_out/(len(train_loader))))
 
 
     def test(epoch):
@@ -179,11 +179,15 @@ def main():
             for batch_idx, (inputs, targets) in enumerate(test_loader):
                 inputs = inputs.to(device)
                 targets = targets.to(device)
-                # mu, sigma = net(inputs)
-                x_out = net(inputs, training_diffusion=False)
-                # x_in = inputs[:,:,-1].view(-1)
-                # x_out = x_in + mu * deltat + sigma * math.sqrt(deltat) * torch.randn_like(x_in)
-                loss = mse(x_out, targets)
+                mu, sigma = net(inputs)
+                
+                current_mu = 0
+                N = 10
+                for i in range(N):
+                    mu, sigma = net(inputs)
+                    current_mu = current_mu + mu
+                current_mu = current_mu / N
+                loss = mse(targets, current_mu)
                 test_loss += loss.item()
 
             print(' Test epoch: {} \tLoss: {:.6f}'
@@ -203,7 +207,7 @@ def main():
 
     if not os.path.isdir('./save_sdenet_wind'):
         os.makedirs('./save_sdenet_wind')
-    torch.save(net.state_dict(),'./save_sdenet_wind/final_model')
+    torch.save(net.state_dict(),'./save_sdenet_wind/final_model_outofdomanin')
 
 
 if __name__ == '__main__':
--- WIND/models/sdenet_wind.py
+++ WIND/models/sdenet_wind.py
@@ -8,157 +8,90 @@ import math
 __all__ = ['SDENet_wind']
 
 
-def init_params(net):
-    '''Init layer parameters.'''
-    for m in net.modules():
-        if isinstance(m, nn.Conv1d):
-            init.kaiming_normal_(m.weight, mode='fan_out')
-            if m.bias is not None:
-                init.constant_(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm1d):
-            init.constant_(m.weight, 1)
-            init.constant_(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            init.normal_(m.weight, std=1e-3)
-            if m.bias is not None:
-                init.constant_(m.bias, 0)
-
-
-def norm(dim):
-    return nn.GroupNorm(min(32, dim), dim)
-
-
-class ConcatConv1d(nn.Module):
-    
-    def __init__(self, dim_in, dim_out, ksize=3, stride=1, padding=0, dilation=1, groups=1, bias=True, transpose=False):
-        super(ConcatConv1d, self).__init__()
-        module = nn.ConvTranspose1d if transpose else nn.Conv1d
-        self._layer = module(   #???
-            dim_in + 1, dim_out, kernel_size=ksize, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias
-        )
-
-    def forward(self, t, x):
-        tt = torch.ones_like(x[:, :1, :]) * t
-        ttx = torch.cat([tt, x], 1)
-        return self._layer(ttx)
-    
-
-class Flatten(nn.Module):
-    
-    def __init__(self):
-        super(Flatten, self).__init__()
-
-    def forward(self, x):
-        shape = torch.prod(torch.tensor(x.shape[1:])).item()
-        return x.view(-1, shape)
+# def init_params(net):
+#     '''Init layer parameters.'''
+#     for m in net.modules():
+#         if isinstance(m, nn.Conv1d):
+#             init.kaiming_normal_(m.weight, mode='fan_out')
+#             if m.bias is not None:
+#                 init.constant_(m.bias, 0)
+#         elif isinstance(m, nn.BatchNorm1d):
+#             init.constant_(m.weight, 1)
+#             init.constant_(m.bias, 0)
+#         elif isinstance(m, nn.Linear):
+#             init.normal_(m.weight, std=1e-3)
+#             if m.bias is not None:
+#                 init.constant_(m.bias, 0)
 
 
 class Drift(nn.Module):
-    
-    def __init__(self, dim):
+    def __init__(self):
         super(Drift, self).__init__()
-        self.norm1 = norm(dim=dim)
+        self.fc = nn.Linear(50, 50)
         self.relu = nn.ReLU(inplace=True)
-        self.conv1 = ConcatConv1d(dim_in=dim, dim_out=dim, ksize=3, stride=1, padding=1)
-        self.norm2 = norm(dim=dim)
-        self.conv2 = ConcatConv1d(dim_in=dim, dim_out=dim, ksize=3, stride=1, padding=1)
-        self.norm3 = norm(dim=dim)
-
+        
     def forward(self, t, x):
-        out = self.norm1(x)
+        out = self.fc(x)
         out = self.relu(out)
-        out = self.conv1(t, out)
-        out = self.norm2(out)
-        out = self.relu(out)
-        out = self.conv2(t, out)
-        out = self.norm3(out)
         return out   
 
 
 class Diffusion(nn.Module):
-    
-    def __init__(self, dim_in, dim_out):
+    def __init__(self):
         super(Diffusion, self).__init__()
-        self.norm1 = norm(dim=dim_in)
         self.relu = nn.ReLU(inplace=True)
-        self.conv1 = ConcatConv1d(dim_in=dim_in, dim_out=dim_out, ksize=3, stride=1, padding=1)
-        self.norm2 = norm(dim_in)
-        self.conv2 = ConcatConv1d(dim_in=dim_in, dim_out=dim_out, ksize=3, stride=1, padding=1)
-        self.fc = nn.Sequential(
-            norm(dim_out), 
-            nn.ReLU(inplace=True), 
-            nn.AdaptiveAvgPool1d(output_size=1),
-            Flatten(), 
-            nn.Linear(in_features=dim_out, out_features=1), 
-            nn.Sigmoid()
-        )
+        self.fc1 = nn.Linear(50, 100)
+        self.fc2 = nn.Linear(100, 1)
         
     def forward(self, t, x):
-        out = self.norm1(x)
+        out = self.fc1(x)
         out = self.relu(out)
-        out = self.conv1(t, out)
-        out = self.norm2(out)
-        out = self.relu(out)
-        out = self.conv2(t, out)
-        out = self.fc(out)
+        out = self.fc2(out)
+        out = torch.sigmoid(out)
         return out
     
 
 class SDENet_wind(nn.Module):
-    
-    def __init__(self, layer_depth, dim=64):
+    def __init__(self, layer_depth, H):
         super(SDENet_wind, self).__init__()
         self.layer_depth = layer_depth
-        self.downsampling_layers = nn.Sequential(
-            #                                                                                                            [N,C,H]
-            #                                                                                                            [128,1,28]
-            nn.Conv1d(in_channels=1, out_channels=dim, kernel_size=3, stride=1, padding=0, padding_mode='replicate'),   #[128,dim,26]
-            norm(dim=dim),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(in_channels=dim, out_channels=dim, kernel_size=4, stride=2, padding=1, padding_mode='replicate'), #[128,dim,13]
-            norm(dim=dim),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(in_channels=dim, out_channels=dim, kernel_size=4, stride=2, padding=1, padding_mode='replicate'), #[128,dim,6]
-        )
-        self.drift = Drift(dim=dim)
-        self.diffusion = Diffusion(dim_in=dim, dim_out=dim)
+        self.downsampling_layers = nn.Linear(H, 50)
+        self.drift = Drift()
+        self.diffusion = Diffusion()
         self.fc_layers = nn.Sequential(
-            norm(dim=dim), 
-            nn.ReLU(inplace=True), 
-            nn.AdaptiveAvgPool1d(output_size=1), 
-            Flatten(), 
-            # nn.Linear(in_features=dim, out_features=2)  # 2 outputs: mu and sigma
-            nn.Linear(in_features=dim, out_features=1)  # 1 output
+            nn.ReLU(inplace=True),
+            nn.Linear(50, 2)
         )
-        self.deltat = 6./self.layer_depth   #? why 6./self.layer_depth
-        self.apply(init_params)
-        self.sigma = 500
+        self.deltat = 4./self.layer_depth
+        # self.apply(init_params)
+        self.sigma = 5
     
     def forward(self, x, training_diffusion=False):
         out = self.downsampling_layers(x)
         if not training_diffusion:
             t = 0
             diffusion_term = self.sigma * self.diffusion(t, out)
-            diffusion_term = torch.unsqueeze(input=diffusion_term, dim=2)
             for i in range(self.layer_depth):
-                t = 6 * (float(i))/self.layer_depth
+                t = 4 * float(i) / self.layer_depth
                 #*
                 #* STOCHASTIC DIFFERENTIAL EQUATION
                 #* (Euler-Maruyama)
-                out = out + self.drift(t,out) * self.deltat + diffusion_term * math.sqrt(self.deltat) * torch.randn_like(out).to(x)
+                out = out \
+                    + self.drift(t,out) * self.deltat \
+                        + diffusion_term * math.sqrt(self.deltat) * torch.randn_like(out).to(x)
             final_out = self.fc_layers(out)
-            return final_out
-            mu = final_out[:,0]
-            sigma = F.softplus(final_out[:,1]) + 1e-3   # smooth approximation of ReLu
+            mu = final_out[:,:,0]
+            sigma = F.softplus(final_out[:,:,1]) + 1e-3
             return mu, sigma
         else:
             t = 0
             final_out = self.diffusion(t, out.detach())
-            return final_out
+            sigma = final_out[:,:,0]
+            return sigma
 
 
 def test():
-    model = SDENet_wind(layer_depth=10, dim=64)
+    model = SDENet_wind(layer_depth=10, H=64)
     return model
  
  
