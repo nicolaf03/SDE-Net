da4190085d8ffe843bab5b4e13aa6a1addf35946
nicolaf03 <nicola.fraccarolo@gmail.com>
2023-12-19 18:05:07 +0100

    [preprocessing] sistemato split dei dataset e esportato residui

--- data/break_data_into_zones.py
+++ data/break_data_into_zones.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from pathlib import Path
 
-ZONES = ["NORD", "CNOR", "CSUD", "SUD", "CALA", "SICI", "SARD"]
+ZONES = ["NORD", "CNOR", "CSUD", "SUD", "SICI", "SARD"]
 
 curr_dir = Path(__file__).parent
 file_path = curr_dir / 'wind_supply_ITA.csv'
 df = pd.read_csv(file_path, delimiter=';', decimal=',', skiprows=2)
 
-for zone in ZONES:
-    sub_df = pd.DataFrame(df.iloc[:,0:2])
-    sub_df.rename(columns={'Date': 'Date', 'KWh (daily average)': 'Energy'}, inplace=True)
+for i in range(len(ZONES)):
+    zone = ZONES[i]
+    sub_df = pd.DataFrame(df.iloc[:,2*i:(2*i+2)])
+    sub_df.rename(columns={sub_df.columns[0]: 'Date', sub_df.columns[1]: 'Energy'}, inplace=True)
     sub_df.dropna(inplace=True)
     sub_df.iloc[:,0] = pd.to_datetime(sub_df.iloc[:,0], format="%d/%m/%Y")
     sub_df.set_index('Date', inplace=True)
